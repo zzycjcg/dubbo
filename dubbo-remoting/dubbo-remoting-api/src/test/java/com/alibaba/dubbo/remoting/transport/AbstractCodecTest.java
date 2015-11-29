f0f5293f724355a73829cdd8ac8c1d88f2ed2c7b
zhiyong zhu <zzycjcg@qq.com>
2015-11-29 22:53:42 +0800

    升级部分依赖版本

--- dubbo-remoting/dubbo-remoting-api/src/test/java/com/alibaba/dubbo/remoting/transport/AbstractCodecTest.java
+++ dubbo-remoting/dubbo-remoting-api/src/test/java/com/alibaba/dubbo/remoting/transport/AbstractCodecTest.java
@@ -15,52 +15,60 @@
  */
 package com.alibaba.dubbo.remoting.transport;
 
-import java.io.IOException;
-
-import com.alibaba.dubbo.common.URL;
-import com.alibaba.dubbo.remoting.Channel;
-import junit.framework.TestCase;
-
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.*;
-import static org.hamcrest.CoreMatchers.*;
+
+import java.io.IOException;
+
+import org.hamcrest.CoreMatchers;
+
+import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.remoting.Channel;
+
+import junit.framework.TestCase;
 
 /**
  * @author ding.lid
  */
-public class AbstractCodecTest extends TestCase {
-
-    public void test_checkPayload_default8M() throws Exception {
+public class AbstractCodecTest extends TestCase
+{
+    
+    public void test_checkPayload_default8M()
+        throws Exception
+    {
         Channel channel = createMock(Channel.class);
         expect(channel.getUrl()).andReturn(URL.valueOf("dubbo://1.1.1.1")).anyTimes();
         replay(channel);
-
+        
         AbstractCodec.checkPayload(channel, 1 * 1024 * 1024);
-
-        try {
+        
+        try
+        {
             AbstractCodec.checkPayload(channel, 15 * 1024 * 1024);
         }
-        catch (IOException expected) {
-            assertThat(expected.getMessage(), allOf(
-                    containsString("Data length too large: "),
-                    containsString("max payload: " + 8 * 1024 * 1024)
-            ));
+        catch (IOException expected)
+        {
+            assertThat(expected.getMessage(),
+                allOf(CoreMatchers.containsString("Data length too large: "),
+                    CoreMatchers.containsString("max payload: " + 8 * 1024 * 1024)));
         }
-
+        
         verify(channel);
     }
-
-    public void test_checkPayload_minusPayloadNoLimit() throws Exception {
+    
+    public void test_checkPayload_minusPayloadNoLimit()
+        throws Exception
+    {
         Channel channel = createMock(Channel.class);
         expect(channel.getUrl()).andReturn(URL.valueOf("dubbo://1.1.1.1?payload=-1")).anyTimes();
         replay(channel);
-
+        
         AbstractCodec.checkPayload(channel, 15 * 1024 * 1024);
-
+        
         verify(channel);
     }
 }
